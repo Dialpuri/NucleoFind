53febc071c86b0bc5bde4b67dab1d6aa199d9d6b
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-04-05 15:00:01 +0100

    Implement auto prediction of phosphate map
    Added a functionality to automatically predict a phosphate map before building the model in 'nucleofind' build script. When the 'predin' argument is set to "auto", the script will run the 'predict_map' function and output a phosphate map into the current working directory. This feature enhances the capability of the 'nucleofind' tool while making it more user-friendly. Additionally, the relevant documentation is updated to reflect this change.

--- package/src/nucleofind/build.py
+++ package/src/nucleofind/build.py
@@ -1,7 +1,7 @@
 from .nautilus_module import Input, Output, run
 import argparse
 from .__version__ import __version__
-
+from .predict import predict_map
 
 def main():
     parser = argparse.ArgumentParser(description='nucleofind build')
@@ -18,14 +18,42 @@ def main():
 
     args = parser.parse_args()
 
-    input = Input(args.mtzin, args.seqin, args.pdbin, args.predin, "", "", args.colin_fo, "", "", args.colin_fc,
+    if args.predin == "auto":
+        print("Before building, NucleoFind will predict a phosphate map and output it into the current working directory")
+        split_calculated_sf = args.colin_fc.split(",")
+
+        if not split_calculated_sf:
+            print("No Calculated SFs provided, attempting to use FWT and PHWT")
+            split_calculated_sf = ["FWT", "PHWT"]
+
+        predict_map(model="phosphate",
+                    input=args.mtzin,
+                    output="phosphate.map",
+                    intensity=split_calculated_sf[0],
+                    phase=split_calculated_sf[1])
+
+        args.predin = "phosphate.map"
+
+    input = Input(args.mtzin,
+                  args.seqin,
+                  args.pdbin,
+                  args.predin,
+                  "",
+                  "",
+                  args.colin_fo,
+                  "",
+                  "",
+                  args.colin_fc,
                   args.colin_free)
+
     output = Output(args.pdbout)
 
     run(input, output, args.cycles)
 
 
 def build(mtzin: str, seqin: str, pdbin: str, predin: str, colin_fo: str, colin_fc: str, colin_free: str, pdbout: str):
+
+
     input = Input(mtzin, seqin, pdbin, predin, "", "", colin_fo, "", "", colin_fc,
                   colin_free)
     output = Output(pdbout)
