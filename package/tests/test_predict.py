1bc77eb7b15152ea1510a629b777b1327f036b34
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-04-10 12:02:22 +0100

    Add new tests and workflow for prediction feature
    Expanded the test suite by adding new tests in 'test_predict.py' to verify the prediction accuracy. Besides, a new GitHub action workflow named 'test.yml' has been created to automate the running of these tests whenever there is a pull request to the master branch.

--- package/tests/test_predict.py
+++ package/tests/test_predict.py
@@ -1,10 +1,11 @@
 import nucleofind.predict as p
 import numpy as np
 import pytest
+import hashlib
 
 @pytest.fixture
 def data():
-    model_path = "../models/phosphate.onnx"
+    model_path = p.find_model("phosphate")
     mtzin = "tests/test_data/5d5w/hklout.mtz"
     pdbin = "tests/test_data/5d5w/xyzout.pdb"
     seqin = "tests/test_data/5d5w/5d5w.fasta"
@@ -13,10 +14,12 @@ def data():
     colinfo = "FP,SIGFP"
     colinfc = "FWT,PHWT"
     colinfree = "FREE"
-    prediction = p.Prediction(model_dir=model_path, use_cache=False)
+    phosphate_map_md5sum = "f9f3470859cac6c976b5146a635c58e7"
+    prediction = p.Prediction(model_dir=model_path)
     return {"model_path": model_path, "mtzin": mtzin, "pdbin": pdbin,
             "seqin": seqin, "pdbout": pdbout, "predin": predin, "colinfo": colinfo,
-            "colinfc": colinfc, "colinfree": colinfree,"prediction": prediction}
+            "colinfc": colinfc, "colinfree": colinfree, "prediction": prediction,
+            "phosphate_map_md5sum": phosphate_map_md5sum}
 
 def test_prediction(data):
     data["prediction"].make_prediction(data["mtzin"], data["colinfc"].split(","), overlap=32)
@@ -26,3 +29,12 @@ def test_prediction(data):
     assert i0 == 0.0
     assert i1 == 1.0
 
+    data["prediction"].save_predicted_map("phosphate.map")
+
+    with open("phosphate.map", "rb") as f:
+        file_hash = hashlib.md5()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+
+        found_sum = file_hash.hexdigest()
+    assert found_sum == data["phosphate_map_md5sum"]
