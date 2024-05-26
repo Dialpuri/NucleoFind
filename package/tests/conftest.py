dded78d6386c8c17d659d689fc3a9cc2ff37f4c9
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2024-05-26 09:26:08 +0100

    Add unit test marker and command line option
    The pytest configuration in `conftest.py` has been updated to include an additional marker for unit tests. This change also introduces a new command-line option (`--unit`) which can be used to include or exclude unit tests during test execution.

--- package/tests/conftest.py
+++ package/tests/conftest.py
@@ -4,17 +4,28 @@ def pytest_addoption(parser):
     parser.addoption(
         "--runslow", action="store_true", default=False, help="run slow tests"
     )
+    parser.addoption(
+        "--unit", action="store_true", default=False, help="run unit tests"
+    )
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "unit: mark test as unit test")
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
+    if not config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+        skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
+
+    if not config.getoption("--unit"):
+        # --unit given in cli: do not skip unit tests
+        skip_unit = pytest.mark.skip(reason="need --unit option to run")
+
+        for item in items:
+            if "unit" in item.keywords:
+                item.add_marker(skip_unit)
