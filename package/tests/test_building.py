0d5db86e9b6c8596abe1293e1fb2e84a9d8a43fa
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2025-03-04 10:34:31 +0000

    Updated building test times

--- package/tests/test_building.py
+++ package/tests/test_building.py
@@ -11,14 +11,14 @@ from pathlib import Path
 def expected_values(test_example):
     data = {
         "1hr2": {
-            "time": 50,
+            "time": 100,
             "fragments_built": 10,
             "residues_built": 250,
             "residues_sequenced": 150,
             "longest_fragment": 45
         },
         "5d5w": {
-            "time": 10,
+            "time": 20,
             "fragments_built": 1,
             "residues_built": 5,
             "residues_sequenced": 4,
